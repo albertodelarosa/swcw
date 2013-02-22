edf228baaa9c0b224fa74acdc26ca0799089a3a5
Alberto de la Rosa <beto@mac.local>
2013-02-22 15:18:01 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sitelerwash::Application.config.secret_token = 'b95563cec91d5409b7f4f15226b0e9f74916f1653a52f2925ef46b737bbe99ff8bac0cc4cf98998099c13686d450a1f95962f951e144559ef569da46d0bee956'
