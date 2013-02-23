455fd73eb937db6d9e08d285c097de575a67e0cf
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2013-02-22 16:14:17 -0800

    added username field & routes for username configuration

--- db/migrate/20130222232554_devise_create_users.rb
+++ db/migrate/20130222232554_devise_create_users.rb
@@ -37,6 +37,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
+    change_table :users do |t|
+      t.string :username, :null => false, :default => ""
+    end
+
     add_index :users, :email,                :unique => true
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :confirmation_token,   :unique => true
