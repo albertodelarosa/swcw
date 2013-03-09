bb9a0fb7ef5fa8d04ccec58f2a55fd7507edf4a4
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2013-03-09 09:57:12 -0800

    and routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Sitelerwash::Application.routes.draw do
+  get "welcome/index"
+
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
 
+  root :to => "welcome#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
