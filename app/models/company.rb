b93c65fa1ebfc2ab80970b8c5253c2586b0038ca
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2013-03-16 12:10:24 -0700

    added associations to customer

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,9 @@
 class Company < ActiveRecord::Base
   attr_accessible :business_id, :company_no, :name
+
+  has_and_belongs_to_many :customers
+  has_and_belongs_to_many :site_locations
+  has_and_belongs_to_many :site_managers
+  has_and_belongs_to_many :van_managers
+
 end
