a860386752bbed0926ddd776232290c37adaafaa
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2013-05-07 14:26:35 -0700

    added associations for company & location

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,16 @@
 class Company < ActiveRecord::Base
   attr_accessible :company_id, :name, :description, :corporate_email_domain, 
                   :siteler_dollars_percentage, :remove_cc_info, :user_reg_allowed, 
-                  :paypal_allowed, :coupons_allowd, :google_checkout_allowed
+                  :paypal_allowed, :coupons_allowd, :google_checkout_allowed, 
+                  :address_attributes
 
-  has_many :clientele
+  has_one :address, :class_name => 'CompanyAddress', :as => :addressable, :dependent => :destroy
+  has_one :contact_info, :class_name => 'CompanyContactInfo', :as => :contactable, :dependent => :destroy
+
+  has_many :clientele, class_name: "Clientele"
   has_many :clients, through: :clientele, source: :users
+
+  accepts_nested_attributes_for :address
+  accepts_nested_attributes_for :contact_info
+
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,10 +1,13 @@
 class Location < ActiveRecord::Base
-  attr_accessible :comments, :latitude, :longitude, :name
+  attr_accessible :comments, :latitude, :longitude, :name, 
+                  :address_attributes
 
   has_one :address, :class_name => 'CompanyAddress', :as => :addressable, :dependent => :destroy
   has_many :companies #through?
   has_many :sites #through?
 #  has_many :clientele
 #  has_many :clients, through: :clientele, source: :users
+
+  accepts_nested_attributes_for :address
 
 end
