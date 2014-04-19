4d7d7f00eebbbb9dc5c3e97509fa8ae19ec95e97
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2014-04-18 20:04:33 -0700

    oops

---
+++ config/environment.rb
@@ -0,0 +1,16 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'betodelarosa',
+  :password => '1952Chevy',
+  :domain => 'enigmatic-inlet-5364.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
+# Initialize the rails application
+Sitelerwash::Application.initialize!
+
