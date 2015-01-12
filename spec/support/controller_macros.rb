5456fa6d471856e0b0ede96ca4f07ef991f3b21f
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-01-11 22:23:09 -0800

    updated rspec macros file

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -7,12 +7,12 @@ module ControllerMacros
   end
 
   def register_user
-    before(:each) do
+#    before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
       @user = FactoryGirl.create(:user)
       @user.confirm! # or set a confirmed_at inside the factory. Only necessary if you are using the confirmable module
       sign_in @user
-    end
+#    end
   end
 
   #def shared_find "find from" do |entity|
