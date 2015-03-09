0748aadb744c49103853c596ea7a359d96272d68
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-03-09 11:36:23 -0700

    upgraded to rails 4

---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,9 @@
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require utilities/moment-with-langs
+//= require utilities/bootstrap-datetimepicker
+//= require utilities/navbar
+//= require_tree ./ie_utilities/
+
