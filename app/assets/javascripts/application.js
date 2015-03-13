1b7612728f430e94cea5339d4c295574703f4757
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-03-12 23:01:25 -0700

    imported bootstrap js for future picking and pulling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-sprockets
+//= require utilities/bootstrap/bootstrap-sprockets
 //= require utilities/moment-with-langs
 //= require utilities/bootstrap-datetimepicker
 //= require utilities/navbar
--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,7 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-sprockets
+//= require utilities/bootstrap/bootstrap-sprockets
 //= require utilities/moment-with-langs
 //= require utilities/bootstrap-datetimepicker
 //= require utilities/navbar
