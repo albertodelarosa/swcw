640694ebd7d7cd43fafc680e5979bdafeff7cb8d
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-03-16 13:08:11 -0700

    seed for prod... change later

--- db/seeds/production/seed.rb
+++ db/seeds/production/seed.rb
@@ -1,9 +1,15 @@
+require "csv"
+
 require File.join(File.dirname(__FILE__), '..', 'shared', 'utilities.rb')
 require File.join(File.dirname(__FILE__), '..', 'shared', 'clear_database.rb')
 require File.join(File.dirname(__FILE__), '..', 'shared', 'create_companies.rb')
 require File.join(File.dirname(__FILE__), '..', 'shared', 'create_sites.rb')
+require File.join(File.dirname(__FILE__), '..', 'shared', 'create_appointments.rb')
+require File.join(File.dirname(__FILE__), '..', 'shared', 'create_vehicle_settings.rb')
+require File.join(File.dirname(__FILE__), '..', 'shared', 'create_washing_services.rb')
+require File.join(File.dirname(__FILE__), '..', 'shared', 'create_discounts.rb')
+#require File.join(File.dirname(__FILE__), '..', 'shared', 'create_siteler_dollars.rb')
 require File.join(File.dirname(__FILE__), '..', 'shared', 'create_admin_user.rb')
 require File.join(File.dirname(__FILE__), '..', 'shared', 'create_developer.rb')
-require File.join(File.dirname(__FILE__), '..', 'shared', 'create_make_model.rb')
-require File.join(File.dirname(__FILE__), '..', 'shared', 'create_production_vehicle_data.rb')
+require File.join(File.dirname(__FILE__), '..', 'shared', 'create_development_associations.rb')
 
