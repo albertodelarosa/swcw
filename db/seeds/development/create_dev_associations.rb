b43962aef8eb663a4a517cbc53ecac033da4becd
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-06-22 20:26:44 -0700

    moved under developer namespace

---
+++ db/seeds/development/create_dev_associations.rb
@@ -0,0 +1,63 @@
+account = Account.first
+develper = User.first
+
+printStarting("ASSOCIATING TO DEFAULT ACCOUNT...")
+
+printStarting(" DEVELOPER")
+account.user = User.first
+printFinished()
+
+printStarting("SITES")
+first_site_id = Site.first.id
+limit = Site.count + first_site_id - 1
+site1 = Site.find(rand(first_site_id..limit))
+site2 = Site.find(rand(first_site_id..limit))
+account.sites << site1 << site2
+printFinished()
+
+printStarting("COMPANIES")
+account.companies << site1.companies[0] << site2.companies[0]
+printFinished()
+
+printStarting("DEFAULT COMPANY ADDRESS FOR DEVELOPER")
+develper.work_address =
+  CompanyAddress.create!(
+    street1:  site1.companies[0].address.street1,
+    street2:  site1.companies[0].address.street2,
+    suite_no: site1.companies[0].address.suite_no,
+    city:     site1.companies[0].address.city,
+    state:    site1.companies[0].address.state,
+    zip:      site1.companies[0].address.zip
+  )
+develper.save!
+printFinished()
+
+printStarting("VEHICLE")
+account.vehicles << Vehicle.first
+printFinished()
+
+
+printStarting("SITELER DOLLARS")
+account.siteler_dollar = SitelerDollar.first
+printFinished()
+
+printStarting("DISCOUNT")
+account.discounts << Discount.first
+printFinished()
+
+printStarting("APPOINTMENTS")
+first_appointment_id = Appointment.first.id
+
+appointment_today =     Appointment.find(first_appointment_id)
+appointment_tomorrow =  Appointment.find(first_appointment_id + 1)
+appointment_last_week = Appointment.find(first_appointment_id + 2)
+
+account.appointments << appointment_last_week << appointment_today  << appointment_tomorrow
+
+printFinished()
+
+account.save!
+
+printFinished()
+
+
---
+++ db/seeds/development/create_developer.rb
@@ -0,0 +1,42 @@
+printStarting("ADDING DEVELOPER USER")
+developer = User.create!(
+  email:      'alberto.g.delrosa+sitelerwash_developer@gmail.com',
+  username:   'developers',
+  password:   'password',
+  last_name:  'de la Rosa',
+  first_name: 'Alberto',
+  salutation: 'Beto'
+)
+developer.skip_confirmation!
+developer.save!
+printFinished()
+
+printStarting("ADDING ALL DEVELOPER ASSOCIATIONS")
+
+printStarting("ASSOCIATING DEVELOPER HOME ADDRESS")
+developer.home_address =
+  HomeAddress.create!(
+    street1: "2527 Ortega Street",
+    apt_no: "12",
+    city: "San Francisco",
+    state: "CA",
+    zip: "94122"
+  )
+printFinished()
+
+printStarting("ASSOCIATING DEVELOPER HOME CONTACT INFO")
+developer.home_contact_info =
+  HomeContactInfo.create!(
+    phone1: "415.661.7226",
+    mobile: "415.845.0274"
+  )
+printFinished()
+
+printStarting("ASSOCIATING DEVELOPER WORK CONTACT INFO")
+developer.work_contact_info =
+  CompanyContactInfo.create!(
+    phone1: "(650) 333-0168",
+    ext: "329"
+  )
+printFinished()
+
