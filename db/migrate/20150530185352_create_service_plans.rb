be4b0e56b417a8f33172e2680b06b677082adcf5
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-06-23 17:42:49 -0700

    fixed foriegn key bug, changed type to package_type

--- db/migrate/20150530185352_create_service_plans.rb
+++ db/migrate/20150530185352_create_service_plans.rb
@@ -3,16 +3,17 @@ class CreateServicePlans < ActiveRecord::Migration
     create_table :service_plans do |t|
       t.string :name,         index: true, null: false, default: ""
       t.string :status,       index: true, null: false, default: ""
-      t.string :type,         index: true, null: false, default: ""
-      t.belongs_to :account,  index: true
+      t.string :package_type, index: true, null: false, default: ""
+      t.belongs_to :account
+      #t.belongs_to :accounts
 
       t.timestamps null: false
     end
     add_index :service_plans, [:name, :status]
-    add_index :service_plans, [:name, :type]
+    add_index :service_plans, [:name, :package_type]
 
-    add_index :service_plans, [:status, :type]
+    add_index :service_plans, [:status, :package_type]
 
-    add_foreign_key :service_plans, :accounts
+    #add_foreign_key :service_plans, :accounts
   end
 end
