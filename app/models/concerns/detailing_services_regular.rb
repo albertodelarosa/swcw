a1f0bf5f807848b377085c4acdc6c2e1e4a70f23
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-06-23 17:49:23 -0700

    changed comp logic for package deal

--- app/models/concerns/detailing_services_regular.rb
+++ app/models/concerns/detailing_services_regular.rb
@@ -7,13 +7,13 @@ module DetailingServicesRegular
   def populate(name, options = {})
     self.name = name
     self.description = Pricing::RegularServices.const_get((name + " service").upcase.tr(" ","_"))
-    if options[:comp]
+    if (options[:comp] == true)
+      self.small_price = 0.00
+      self.large_price = 0.00
+    else
       price_sizes = Pricing::RegularServices.const_get((name + " price").upcase.tr(" ","_"))
       self.small_price = price_sizes["Small"]
       self.large_price = price_sizes["Large"]
-    else
-      self.small_price = 0.00
-      self.small_price = 0.00
     end
     self.duration = Pricing::RegularServices.const_get((name + " duration").upcase.tr(" ","_"))
   end
