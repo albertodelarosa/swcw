40cd59e86a403f57088721198099fb751bdda2b4
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-06-25 01:03:22 -0700

    fixed turbolinks refreshing-needed bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require utilities/bootstrap/bootstrap-sprockets
 //= require utilities/moment-with-langs
 //= require utilities/bootstrap-datetimepicker
