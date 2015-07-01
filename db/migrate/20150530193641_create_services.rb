144d7e1b3176fb307afc56331a0449f93d86beba
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-07-01 02:00:28 -0700

    renamed chosen_price to price for simplicit's sake

--- db/migrate/20150530193641_create_services.rb
+++ db/migrate/20150530193641_create_services.rb
@@ -3,7 +3,7 @@ class CreateServices < ActiveRecord::Migration
     create_table :services do |t|
       t.string  :name,            index: true, null: false, default: ""
       t.text    :description,     index: true
-      t.decimal :chosen_price,    index: true, null: false, default: 0.0, precision: 8, scale: 2
+      t.decimal :price,           index: true, null: false, default: 0.0, precision: 8, scale: 2
       t.decimal :small_price,     index: true, null: false, default: 0.0, precision: 8, scale: 2
       t.decimal :large_price,     index: true, null: false, default: 0.0, precision: 8, scale: 2
       t.integer :duration,        index: true, null: false, default: 0
@@ -16,19 +16,19 @@ class CreateServices < ActiveRecord::Migration
       t.timestamps null: false
     end
     add_index :services, [:name, :description]
-    add_index :services, [:name, :chosen_price]
+    add_index :services, [:name, :price]
     add_index :services, [:name, :small_price]
     add_index :services, [:name, :large_price]
     add_index :services, [:name, :duration]
 
-    add_index :services, [:description, :chosen_price]
+    add_index :services, [:description, :price]
     add_index :services, [:description, :small_price]
     add_index :services, [:description, :large_price]
     add_index :services, [:description, :duration]
 
-    add_index :services, [:chosen_price, :small_price]
-    add_index :services, [:chosen_price, :large_price]
-    add_index :services, [:chosen_price, :duration]
+    add_index :services, [:price, :small_price]
+    add_index :services, [:price, :large_price]
+    add_index :services, [:price, :duration]
 
     add_index :services, [:small_price, :large_price]
     add_index :services, [:small_price, :duration]
