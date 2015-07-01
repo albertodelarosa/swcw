087ee28fc494ad72d4c50f5da49d5f20384ff22a
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-07-01 14:54:08 -0700

    fixed line item-to service plan- to cart bug, added servce plan params

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,8 +1,36 @@
 class LineItemsController < ApplicationController
   def create
-    @product = Product.find(params[:product_id])
-    @line_item = LineItem.create!(:cart => current_cart, :product => @product, :quantity => 1, :unit_price => @product.price)
-    flash[:notice] = "Added #{@product.name} to cart."
-    redirect_to current_cart_url
+    @service_plan = create_plan(params_service_plan)
+
+    if @service_plan.save!
+      @line_item = LineItem.create!(quantity: 1, unit_price: @service_plan.price)
+      @line_item.cart = current_cart
+      @line_item.service_plan = @service_plan
+      @line_item.save!
+      flash[:notice] = "Added #{@service_plan.name} to cart."
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def params_service_plan
+    plan_params = params.require(:package).permit(:package_name, :package_type, :vehicle_size, :package_price)
+    return plan_params
+  end
+
+  def create_plan(plan_params)
+    package_name = plan_params[:package_name]
+    package_type = plan_params[:package_type]
+    package_price = plan_params[:package_price]
+    vehicle_size = plan_params[:vehicle_size]
+
+    plan = ServicePlan.new
+    options = {package_name: package_name, package_type: package_type, service_name: package_name}
+    plan.populate_regular_plan(options)
+    plan.vehicle_size = vehicle_size
+    plan.set_prices()
+    plan.price = package_price
+    return plan
   end
 end
