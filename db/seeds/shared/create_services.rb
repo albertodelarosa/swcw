1fe1306d7583442800a6e453d89885d7fe4041f7
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-07-01 02:40:41 -0700

    fixed individual service plan bug

--- db/seeds/shared/create_services.rb
+++ db/seeds/shared/create_services.rb
@@ -5,9 +5,6 @@ Pricing::RegularServices::SERVICE_NAMES.each_with_index do |service_name, index|
   temp_plan = ServicePlan.create!()
   temp_plan.populate_regular_plan(options)
   temp_plan.save!
-  #temp_service = ServiceRegular.create!()
-  #temp_service.populate(service_name)
-  #temp_service.save!
 end
 
 printFinished()
