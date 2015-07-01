58d758c569f9c742c0d0c0bf01f11ed7f478a72a
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-07-01 02:29:28 -0700

    created size_set method for system-wide checks-and-balances

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,5 +3,8 @@ class Vehicle < ActiveRecord::Base
   has_many :locations
   has_many :accounts, through: :locations
 
+  def size_set?
+    return self.vehicle_size.empty? ? false : true
+  end
 end
 
