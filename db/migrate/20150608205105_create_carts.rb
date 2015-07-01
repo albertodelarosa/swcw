43f94afb07c4ceb550467a2e7f45925744c4bc34
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-07-01 02:02:28 -0700

    fixed purchased_at bug

--- db/migrate/20150608205105_create_carts.rb
+++ db/migrate/20150608205105_create_carts.rb
@@ -1,7 +1,7 @@
 class CreateCarts < ActiveRecord::Migration
   def change
     create_table :carts do |t|
-      t.datetime :purchased_at, index: true, null: false, default: "2015-01-01 00:00:00"
+      t.datetime :purchased_at, index: true
 
       t.timestamps null: false
     end
