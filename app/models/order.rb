0247d835ece6c535790b399feb81a39f1f418620
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-07-09 20:55:20 -0700

    prep orders for cc submission

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,10 +2,12 @@ class Order < ActiveRecord::Base
   belongs_to :cart
   has_many :transactions, class_name: "OrderTransaction"
 
+  attr_accessor :card_number, :card_verification
+
   validate :validate_card, on: :create
 
   def purchase
-    response = GATEWAY.purchase(price_in_cents, credit_card, purchase_options)
+    response = QB_GATEWAY.purchase(price_in_cents, credit_card, purchase_options)
     transactions.create!(action: "purchase", amount: price_in_cents, response: response)
     cart.update_attribute(:purchased_at, Time.now) if response.success?
     response.success?
@@ -19,8 +21,8 @@ class Order < ActiveRecord::Base
 
   def purchase_options
     {
-      :ip => ip_address,
-      :billing_address => {
+      ip: ip_address,
+      billing_address: {
         :name     => "Ryan Bates",
         :address1 => "123 Main St.",
         :city     => "New York",
@@ -34,14 +36,14 @@ class Order < ActiveRecord::Base
   def validate_card
     unless credit_card.valid?
       credit_card.errors.full_messages.each do |message|
-        errors.add_to_base message
+        errors[:base] << message
       end
     end
   end
 
   def credit_card
     @credit_card ||= ActiveMerchant::Billing::CreditCard.new(
-      :type               => card_type,
+      :brand              => card_type,
       :number             => card_number,
       :verification_value => card_verification,
       :month              => card_expires_on.month,
