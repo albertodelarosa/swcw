ea4ecbe661b43932dadd236dab2ddcb040252cc4
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-07-09 20:46:48 -0700

    fixed omniauth registration & login for facebook... more to come

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,51 @@ class User < ActiveRecord::Base
   accepts_nested_attributes_for :home_address
   accepts_nested_attributes_for :home_contact_info
 
+  def self.from_omniauth(auth)
+
+    user = where(provider: auth.provider, uid: auth.uid).first
+    return user unless user.nil?
+
+    user = where(email: auth.info.email).first
+    unless user.nil?
+      user.provider = auth.provider
+      user.uid = auth.uid
+      if auth.provider.include?("facebook")
+        user.name = auth.info.name if user.name.nil?
+        user.image_url = auth.info.image
+        user.urls = auth.info.urls.Facebook
+      end
+      return user
+    end
+
+    user = create do |created_user|
+      created_user.provider = auth.provider
+      created_user.uid = auth.uid
+      created_user.password = Devise.friendly_token[0,20]
+
+      created_user.email = auth.info.email
+      created_user.first_name = auth.info.first_name
+      created_user.last_name = auth.info.last_name
+
+      if auth.provider.include?("facebook")
+        created_user.name = auth.info.name
+        created_user.image_url = auth.info.image
+        created_user.urls = auth.info.urls.Facebook
+      end
+    end
+    return user
+  end
+
+  #attr_accessor :login
+
+  #def self.find_for_database_authentication(warden_conditions)
+    #conditions = warden_conditions.dup
+    #conditions[:email].downcase! if conditions[:email]
+    #if login = conditions.delete(:login)
+      #where(conditions.to_hash).where(["lower(username) = :value OR lower(email) = :value", { value: login.downcase }]).first
+    #else
+      #where(conditions.to_hash).first
+    #end
+  #end
+
 end
