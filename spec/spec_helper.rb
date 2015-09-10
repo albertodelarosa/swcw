1bc5b1c8f9895cece9abe3075bdf74a73139555f
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2015-09-10 08:57:53 -0700

    upadated factories settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require "capybara/rspec"
+require 'factory_girl_rails'
 #require File.dirname(__FILE__) + "/support/controller_macros"
 
 # Requires supporting ruby files with custom matchers and macros, etc,
@@ -10,6 +11,7 @@ require "capybara/rspec"
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  #config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, :type => :controller
   config.include ControllerMacros, :type => :controller
   config.infer_spec_type_from_file_location!
