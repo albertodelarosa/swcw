dda2a484a727bb31f7a52df1b521de36aab68834
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2016-05-15 18:26:04 -0700

    removed quiet assets paths

--- config/application.rb
+++ config/application.rb
@@ -66,6 +66,6 @@ module Sitelerwash
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-
+    # config.quiet_assets_paths << '/shared/' << '/devise/' << '/welcome/'
   end
 end
