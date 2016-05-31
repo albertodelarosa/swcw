b63d6ec87fa76455a3536bfd8f406545a4bb5473
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2016-05-31 12:38:27 -0700

    updated siteler_dollars spec and factory

--- spec/factories/siteler_dollars.rb
+++ spec/factories/siteler_dollars.rb
@@ -1,6 +1,13 @@
 FactoryGirl.define do
   factory :siteler_dollar do
-    
+    name   "sDollars"
+    amount_paid 9.99
+    bonus       9.99
+    percentage  10
+    total       199.99
+    remaining   129.99
+
+    association :account, factory: :account, strategy: :build
   end
 
 end
--- spec/models/siteler_dollar_spec.rb
+++ spec/models/siteler_dollar_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe SitelerDollar, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  me = FactoryGirl.create(:siteler_dollar)
+
+  has_one_associations = {
+    account: FactoryGirl.create(:account)
+  }
+
+  my_parameters = {
+    name:        [ "String", "sDollars" ],
+    amount_paid: [ "BigDecimal", 9.99],
+    bonus:       [ "BigDecimal", 9.99 ],
+    percentage:  [ "BigDecimal", 10 ],
+    total:       [ "BigDecimal", 199.99 ],
+    remaining:   [ "BigDecimal", 129.99 ]
+  }
+
+  context "HAPPY PATH" do
+
+    describe "base parameters" do
+      it_behaves_like "it is valid object", me, my_parameters
+      it_behaves_like "has_one associations", me, has_one_associations
+    end
+
+  end
+
 end
+
