97294e869e8d3d86afa316becd4cbd5ea3749f45
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2016-06-01 01:03:21 -0700

    updated service_plan spec & factory

--- spec/factories/service_plans.rb
+++ spec/factories/service_plans.rb
@@ -1,16 +1,18 @@
 FactoryGirl.define do
   factory :service_plan do
-    name          "Palace Interior"
-    status        "new"
-    package_type  "Plan"
-    price         129.99
-    vehicle_size  "Large"
+    name         "Palace Interior"
+    status       "new"
+    plan_type    "Plan"
+    price        129.99
+    price_small  129.99
+    price_large  129.99
+    vehicle_size "Small"
+    image_url    "/assets/dashboard/purchase/packages/Palaces1-Interior_PNG_White_500x500.png"
+
+    association :line_item, factory: :line_item, strategy: :build
 
     after do |service_plan|
-      service_plan.account   ||= FactoryGirl.create(:account)
-
-      service_plan.line_item ||= FactoryGirl.create(:line_item)
-      service_plan.services   << FactoryGirl.create(:service)
+      service_plan.services << FactoryGirl.create(:service)
     end
   end
 
--- spec/models/service_plan_spec.rb
+++ spec/models/service_plan_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe ServicePlan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'happy path' do
+
+    me = FactoryGirl.create(:service_plan)
+
+    my_parameters = {
+        name:        [ "String",     "Palace Interior" ],
+        status:      [ "String",     "new" ],
+        plan_type:   [ "String",     "Plan" ],
+        price:       [ "BigDecimal", 129.99 ],
+        price_small: [ "BigDecimal", 129.99 ],
+        price_large: [ "BigDecimal", 129.99 ],
+        vehicle_size:[ "String",     "Small" ],
+        image_url:   [ "String",     "/assets/dashboard/purchase/packages/Palaces1-Interior_PNG_White_500x500.png" ]
+    }
+    has_one_associations = {
+      line_item: FactoryGirl.create(:line_item)
+    }
+
+    has_many_associations = {
+      services:       FactoryGirl.create(:service)
+    }
+
+    describe "base parameters" do
+      it_behaves_like "it is valid object", me, my_parameters
+      it_behaves_like "has_one associations", me, has_one_associations
+      it_behaves_like "has_many associations", me, has_many_associations
+     end
+
+    describe '~>expect to have constants:' do
+      it{ expect( ServicePlan ).to have_constant(:PLAN_TYPE) }
+      it{ expect( ServicePlan ).to have_constant(:STATUS) }
+      it{ expect( ServicePlan ).to have_constant(:PLAN_NAMES) }
+    end
+
+    describe '~>expect to have Palace Complete Plan constants:' do
+      it{ expect( ServicePlan ).to have_constant(:PALACE_COMPLETE_PLAN) }
+      it{ expect( ServicePlan ).to have_constant(:PALACE_COMPLETE_PLAN_PRICE) }
+      it{ expect( ServicePlan ).to have_constant(:PALACE_COMPLETE_PLAN_IMAGE_URL) }
+    end
+
+    describe '~>expect to have Palace Exterior Plan constants:' do
+      it{ expect( ServicePlan ).to have_constant(:PALACE_EXTERIOR_PLAN) }
+      it{ expect( ServicePlan ).to have_constant(:PALACE_EXTERIOR_PLAN_PRICE) }
+      it{ expect( ServicePlan ).to have_constant(:PALACE_EXTERIOR_PLAN_IMAGE_URL) }
+    end
+
+    describe '~>expect to have Palace Interior Plan constants:' do
+      it{ expect( ServicePlan ).to have_constant(:PALACE_INTERIOR_PLAN) }
+      it{ expect( ServicePlan ).to have_constant(:PALACE_INTERIOR_PLAN_PRICE) }
+      it{ expect( ServicePlan ).to have_constant(:PALACE_INTERIOR_PLAN_IMAGE_URL) }
+    end
+
+  end
+
 end
