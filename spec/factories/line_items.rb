1214336d434ee15e3036f11d0935b11dd8241658
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2016-06-01 02:39:01 -0700

    updated line_item spec & factory

--- spec/factories/line_items.rb
+++ spec/factories/line_items.rb
@@ -1,12 +1,7 @@
 FactoryGirl.define do
   factory :line_item do
-    unit_price "9.99"
-    quantity 1
-
-    after do |line_item|
-      line_item.service_plan << FactoryGirl.create(:service_plan).id
-      line_item.cart_id << FactoryGirl.create(:cart)
-    end
+    unit_price 9.99
+    quantity   1
   end
 
 end
--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe LineItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '=>happy path' do
+
+    me = FactoryGirl.create(:line_item)
+
+    my_parameters = {
+      unit_price: [ "BigDecimal", 9.99 ],
+        quantity: [ "Fixnum",     1 ]
+    }
+
+    describe "~>base parameters:" do
+      it_behaves_like "it is valid object", me, my_parameters
+     end
+
+  end
+
 end
