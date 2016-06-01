203948facafb9141a3923cb2e4874edecd4f11f7
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2016-06-01 02:38:11 -0700

    updated discount spec & factory

--- spec/factories/discounts.rb
+++ spec/factories/discounts.rb
@@ -1,6 +1,13 @@
 FactoryGirl.define do
   factory :discount do
-    
+    name        "Limited time 10% off"
+    percentage  10.0
+    applied     false
+    expires     "2015-05-08"
+    expireable  true
+
+    #association :transaction, factory: :transaction, strategy: :build
+
   end
 
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Discount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '=>happy path' do
+
+    me = FactoryGirl.create(:discount)
+
+    my_parameters = {
+        name:       [ "String",     "Limited time 10% off" ],
+        percentage: [ "BigDecimal", 10.0 ],
+        applied:    [ "FalseClass", false ],
+        expireable: [ "TrueClass",  true ],
+        expires:    [ "Date",       Date.new(2015,5,8) ]
+    }
+
+    #has_one_associations = {
+      #transaction: FactoryGirl.create(:transaction)
+    #}
+
+    describe "~>base parameters:" do
+      it_behaves_like "it is valid object", me, my_parameters
+      #it_behaves_like "has_one associations", me, has_one_associations
+     end
+
+  end
+
 end
