d51a5971470b26166287e7fc96deb59f0a122d4e
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2016-06-29 19:04:51 -0700

    bootstrap revamp with material design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,21 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require utilities/bootstrap/bootstrap-sprockets
+//= require utilities/bootstrap
+//= require utilities/jquery.nouislider.min
+//= require utilities/snackbar.min
+//= require utilities/ripples
+//= require utilities/material
+//= require utilities/toolkit.min
 //= require utilities/moment-with-langs
 //= require utilities/bootstrap-datetimepicker
 //= require utilities/navbar
 //= require_tree ./ie_utilities/
+//= require utilities/parallax
+//- require utilities/background_slider
 //= require_tree ./dashboard/
+
+$(document).ready(function(){
+  $.material.init();
+});
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  before_filter :authenticate_user!, :except => [:index]
+  before_filter :authenticate_user!, :except => [:index, :theme, :design_material_theme]
 
   def index
     #add_breadcrumb 'Hotel Manager', '<a href="/hotelmanager/">Hotel Manager</a>'
@@ -7,5 +7,10 @@ class WelcomeController < ApplicationController
     if user_signed_in?
       redirect_to dashboard_root_path()
     end
+  end
+
+  def theme
+  end
+  def design_material_theme
   end
 end
