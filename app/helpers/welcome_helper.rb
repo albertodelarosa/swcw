a2973def5e170d577c13e175e16d0ad4f6589c9f
Alberto de la Rosa <alberto.g.delarosa@gmail.com>
2016-12-02 17:54:28 -0800

    optimized sections

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,3 +1,6 @@
+include ActionView::Helpers::TagHelper
+include ActionView::Context
+
 module WelcomeHelper
   def resource_name
     :user
@@ -7,7 +10,28 @@ module WelcomeHelper
     @resource ||= User.new
   end
 
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
+  def li_nested_ul(title="", &block )
+    if title.empty?
+      return content_tag( :li, content_tag( :ul,  block ) )
+    else
+      return content_tag :li do
+        content_tag :ul do
+          content_tag( :p, title )
+          block
+        end
+      end
+    end
   end
+
+  def li_content ( li )
+     return content_tag( :li, content_tag( :p, li ) )
+  end
+
+  def li_lead_content ( small, p )
+     return content_tag :li do
+       content_tag( :p, nil, { class: "lead" }, content_tag( :small, small ) )
+       content_tag( :p, p )
+     end
+  end
+
 end
